53ec3ee73f9657af2d2bd15f2bf4dfe3cf1102f4
Moinul48 <mhmoinul@gmail.com>
2021-02-14 20:27:56 +0600

    restrict slider change duration

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ const selectItem = (event, img) => {
     sliders.push(img);
   } else {
     sliders.pop(img);
-    // alert('Hey, Already added !')
   }
 }
 var timer
@@ -69,18 +68,28 @@ const createSlider = () => {
 
   sliderContainer.appendChild(prevNext)
   document.querySelector('.main').style.display = 'block';
+
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+
+  // restrict slider change duration less then 1 second
+  const slideDuration = document.getElementById('duration');
+  const duration = slideDuration.value || 1000;
+  if(duration >= 1000){
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item)
+    })
+  }
+  else{
+    alert('Slider change duration at lest 1000 millisecond!!!')
+    return;
+  }
   
-  sliders.forEach(slide => {
-    let item = document.createElement('div')
-    item.className = "slider-item";
-    item.innerHTML = `<img class="w-100"
-    src="${slide}"
-    alt="">`;
-    sliderContainer.appendChild(item)
-  })
   changeSlide(0)
   timer = setInterval(function () {
     slideIndex++;
@@ -125,3 +134,9 @@ searchBtn.addEventListener('click', function () {
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+
+const toggleElement = () => {
+  const spinner = document.getElementById("loading-spinner");
+
+
+}
